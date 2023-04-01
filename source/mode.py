25366c96c9cc2472eaf4ad3e72ae63c15c15a7a1
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-04-01 19:11:56 +0200

    docstring

--- source/mode.py
+++ source/mode.py
@@ -1,11 +1,24 @@
 # mode.py
 from collections import Counter
+import typing
 
 
 def idk(a, b):
   return a * b
 
-def mode(data):
+def mode(data: typing.Sequence[typing.Any]) -> list[typing.Any]:
+  """_summary_
+
+  Parameters
+  ----------
+  data : typing.Sequence[typing.Any]
+      _description_
+
+  Returns
+  -------
+  list[typing.Any]
+      _description_
+  """
   counter = Counter(data)
   _, top_count = counter.most_common(1)[0]
   return [point for point, count in counter.items() if count == top_count]
