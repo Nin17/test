77b954b3e3393fb847e856cdfb10de7b312d3f27
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-04-01 18:43:43 +0200

    relative dirs

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../source'))
-sys.path.insert(0, os.path.abspath('_ext'))
+sys.path.insert(0, os.path.abspath('../_ext'))
 
 
 # -- General configuration ------------------------------------------------
