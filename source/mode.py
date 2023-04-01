f127da9aae1576885c35c45a5a63dc36224d7149
Chris Ninham <61634310+Nin17@users.noreply.github.com>
2023-04-01 19:28:19 +0200

    add more functions

--- source/mode.py
+++ source/mode.py
@@ -23,3 +23,53 @@ def mode(data: typing.Sequence[typing.Any]) -> list[typing.Any]:
   _, top_count = counter.most_common(1)[0]
   return [point for point, count in counter.items() if count == top_count]
 
+def mode2(data: typing.Sequence[typing.Any]) -> list[typing.Any]:
+  """_summary_
+
+  Parameters
+  ----------
+  data : typing.Sequence[typing.Any]
+      _description_
+
+  Returns
+  -------
+  list[typing.Any]
+      _description_
+  """
+  counter = Counter(data)
+  _, top_count = counter.most_common(1)[0]
+  return [point for point, count in counter.items() if count == top_count]
+
+def mode3(data: typing.Sequence[typing.Any]) -> list[typing.Any]:
+  """_summary_
+
+  Parameters
+  ----------
+  data : typing.Sequence[typing.Any]
+      _description_
+
+  Returns
+  -------
+  list[typing.Any]
+      _description_
+  """
+  counter = Counter(data)
+  _, top_count = counter.most_common(1)[0]
+  return [point for point, count in counter.items() if count == top_count]
+
+def mode4(data: typing.Sequence[typing.Any]) -> list[typing.Any]:
+  """_summary_
+
+  Parameters
+  ----------
+  data : typing.Sequence[typing.Any]
+      _description_
+
+  Returns
+  -------
+  list[typing.Any]
+      _description_
+  """
+  counter = Counter(data)
+  _, top_count = counter.most_common(1)[0]
+  return [point for point, count in counter.items() if count == top_count]
